864c63db3c6a1176fba234d82631bccf52c24624
Hasinur <hasinur.rahman1332@gmail.com>
2025-02-11 19:20:34 +0600

    dashboard layout UI

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,51 @@
-const Sidebar = () => {
-  return <div>sidebar</div>;
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
+  return (
+    <div
+      className={`fixed z-50 top-0 left-0 h-full bg-gray-900 border-r p-4 transform ${
+        isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 `}
+    >
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <a
+            href="https://hasinur.netlify.app"
+            target="_blank"
+            className="text-2xl md:text-3xl text-blue-500 font-extrabold tracking-wide"
+          >
+            Portfolio
+          </a>
+        </div>
+        {/* Close Button for Desktop */}
+        <button
+          className="text-white cursor-pointer p-3 rounded-r-full bg-primaryColor fixed -right-11 top-0 md:block"
+          onClick={toggleSidebar}
+        >
+          {isSidebarOpen && <FaTimes className="text-xl" />}
+          {!isSidebarOpen && <FaBars className="text-xl" />}
+        </button>
+      </div>
+      <hr className="my-1 border-white rounded-full border" />
+
+      {/* Menu */}
+      <h3 className="font-semibold text-lg mt-5 text-white">Menu</h3>
+
+      {/* List */}
+      <div className="flex flex-col space-y-4 h-full">
+        <ul className="mt-4 space-y-2 text-white">
+          <li>
+            <Link to={"/dashboard"}>Projects</Link>
+          </li>
+          <li>
+            <Link to={"/dashboard/add-project"}>Add Projects</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
+
+import Sidebar from "../Components/Sidebar/Sidebar";
 
 const RootLayout = () => {
-  return (
-    <div>
-      {/* sidebar */}
-      <div>sidebar</div>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-      {/* outlet */}
-      <Outlet />
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="w-full min-h-screen bg-gray-800 flex overflow-x-hidden">
+      {/* Sidebar */}
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+      {/* Main Content */}
+      <div
+        className={`flex-1 p-4 transition-all duration-300 overflow-y-auto ${
+          isSidebarOpen ? "md:ml-64" : "md:ml-0"
+        }`}
+      >
+        <Outlet />
+      </div>
     </div>
   );
 };
